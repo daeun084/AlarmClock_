ed3537f5db85554dea352ee047ded323258500a6
daeun084 <030804jk@naver.com>
2023-09-09 15:06:30 +0900

    StopWatch 기능 구현

--- AlarmClock_/StopWatch/StopWatchVC.swift
+++ AlarmClock_/StopWatch/StopWatchVC.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 class StopWatchVC : UIViewController {
-    
-   // var timer = scheduledTimer(withTimeInterval: 1, repeats: true, block: 1)
-    
-    var isPlaying : Bool?
+        
+    var timeLabel : UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = UIFont.systemFont(ofSize: 85, weight: .ultraLight)
+        label.textAlignment = .center
+        return label
+    }()
+    var stopWatchTimer : Timer?
+    var elapsedTime : TimeInterval = 0
     
     let labBtn : UIButton = {
         let btn = UIButton()
-        var title = AttributedString("랩")
+        var title = AttributedString("Lab")
         var config = UIButton.Configuration.filled()
         config.cornerStyle = .capsule
         config.attributedTitle = title
@@ -26,12 +32,11 @@ class StopWatchVC : UIViewController {
     
     let startBtn : UIButton = {
         let btn = UIButton()
-        var title = AttributedString("시작")
+        var title = AttributedString("Start")
         var config = UIButton.Configuration.filled()
         config.cornerStyle = .capsule
         config.attributedTitle = title
-        config.baseBackgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
-        //config.baseForegroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        config.baseBackgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
         btn.configuration = config
         return btn
     }()
@@ -41,21 +46,30 @@ class StopWatchVC : UIViewController {
         overrideUserInterfaceStyle = .dark
         makeSubView()
         makeConstraint()
+        makeAddTarget()
+        timeLabel.text = "0:00:00"
     }
     
 }
 
 extension StopWatchVC {
     func makeSubView(){
+        view.addSubview(timeLabel)
         view.addSubview(labBtn)
         view.addSubview(startBtn)
     }
     
     func makeConstraint(){
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
         labBtn.translatesAutoresizingMaskIntoConstraints = false
         startBtn.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            labBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            
+            timeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70),
+            timeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            timeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            
+            labBtn.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 70),
             labBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             labBtn.widthAnchor.constraint(equalToConstant: 80),
             labBtn.heightAnchor.constraint(equalTo: labBtn.widthAnchor),
@@ -68,7 +82,43 @@ extension StopWatchVC {
         ])
     }
     
+    func makeAddTarget(){
+        startBtn.addTarget(self, action: #selector(setStartBtnFunc(sender:)), for: .allEvents)
+    }
     
+    @objc func setStartBtnFunc(sender : UIButton){
+        if let timer = stopWatchTimer {
+            //timer가 running중이면 멈추기
+            
+            elapsedTime = 0
+            stopWatchTimer = nil
+            timer.invalidate()
+            sender.configuration?.baseBackgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
+            sender.setTitle("Start", for: .normal)
+            
+            
+            //초기화 기능 지우고
+            //Lab 버튼을 재설정으로 수정 -> 누르면 초기화
+            //running 중일 때 Lab btn 누르면 시간 기록
+            //stop 버튼 누르면 lab btn - reset btn
+            
+        }
+        else{
+            //timer가 running중이지 않다면 watch 활성화
+            stopWatchTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [weak self] timer in
+                self?.elapsedTime += timer.timeInterval
+
+                let minutes = Int(self!.elapsedTime) / 60 % 60
+                let seconds = Int(self!.elapsedTime) % 60
+                let milliseconds = Int(self!.elapsedTime * 100) % 100
+                               
+                self?.timeLabel.text = String(format: "%02d:%02d:%02d", minutes, seconds, milliseconds)
+                           
+            })
+            sender.configuration?.baseBackgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+            sender.setTitle("Stop", for: .normal)
+    }
+        
     
-    
+    }
 }
